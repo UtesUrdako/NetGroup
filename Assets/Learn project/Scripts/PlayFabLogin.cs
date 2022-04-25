8900bdf55710841cf266e1c4dce9ad458edfcef2
utesurdako <arktnels@gmail.com>
2022-04-25 22:32:18 +0300

    Learn 5 and shaders

--- Assets/Learn project/Scripts/PlayFabLogin.cs
+++ Assets/Learn project/Scripts/PlayFabLogin.cs
@@ -12,19 +12,14 @@ public class PlayFabLogin : MonoBehaviour
      
      public void Start()
      {
-          // Here we need to check whether TitleId property is configured in settings or not
           if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
-          {
-               /*
-               * If not we need to assign it to the appropriate variable manually
-               * Otherwise we can just remove this if statement at all
-               */
                PlayFabSettings.staticSettings.TitleId = "12D7F";
-          }
 
           var isNeedCreate = PlayerPrefs.HasKey(AutorizationIdKey);
           _guid = PlayerPrefs.GetString(AutorizationIdKey, Guid.NewGuid().ToString());
+          
           var request = new LoginWithCustomIDRequest { CustomId = _guid, CreateAccount = !isNeedCreate };
+          
           PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
      }
      private void OnLoginSuccess(LoginResult result)
@@ -32,21 +27,7 @@ public class PlayFabLogin : MonoBehaviour
           Debug.Log($"Congratulations, you made successful API call! ID = {_guid}");
           PlayerPrefs.SetString(AutorizationIdKey, _guid);
           
-          PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(),
-          result =>
-          {
-               Debug.Log($"PlayFab ID: {result.AccountInfo.PlayFabId}");
-               Social.localUser.Authenticate(resultSocial =>
-               {
-                    if (resultSocial)
-                         result.AccountInfo.GoogleInfo.GoogleId = Social.Active.localUser.id;
-               });
-          }, error =>
-          {
-               var errorMessage = error.GenerateErrorReport();
-               Debug.LogError($"Something went wrong: {errorMessage}");
-          });
-          SceneManager.LoadScene(1);
+          SceneManager.LoadScene("Lobby");
      }
      
      private void OnLoginFailure(PlayFabError error)
